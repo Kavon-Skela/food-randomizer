1238b79273499bba89fb9933cb5e3813dcccd9c3
Aleksandr <alexnovaklawyer@gmail.com>
2024-01-10 00:18:24 +0200

    only one option left

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,15 @@
 <script>
   import { writable } from "svelte/store";
   import AutoMode from "../Components/AutoMode/AutoMode.svelte";
-  import ChooseMode from "../Components/ChooseMode/ChooseMode.svelte";
-  import { afterUpdate } from "svelte";
 
   let mode = writable('Абсолютний рандом');
-
-  const handleChoose = ({ currentTarget }) => {
-    $mode = currentTarget.innerText;
-  };
 </script>
 
 <main class="main">
-  <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      {$mode}
-    </button>
-    <ul class="dropdown-menu">
-      <li><button class="dropdown-item" on:click={handleChoose}>Абсолютний рандом</button></li>
-      <li><button class="dropdown-item" on:click={handleChoose}>Оберу зі списку</button></li>
-    </ul>
-  </div>
+  <h3>Абсолютний рандом</h3>
 
   <section>
-    {#if $mode === 'Абсолютний рандом'}
       <AutoMode />
-    {:else if $mode === 'Оберу зі списку'}
-      <ChooseMode />
-    {/if}
   </section>
 </main>
 
@@ -37,9 +19,5 @@
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
-    .dropdown {
-      margin: 20px;
-    }
   }
 </style>
